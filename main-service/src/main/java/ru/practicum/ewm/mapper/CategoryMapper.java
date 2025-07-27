edfba186f276109dd1cc64d76ce0c06a98a61a60
kutanig <anatolijkutarev@gmail.com>
2025-07-27 04:51:20 +0200

    v0.3

---
+++ main-service/src/main/java/ru/practicum/ewm/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package ru.practicum.ewm.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.ewm.dto.category.CategoryDto;
+import ru.practicum.ewm.model.category.Category;
+
+@Component
+public class CategoryMapper {
+    private CategoryDto mapToCategoryDto(Category category) {
+        return new CategoryDto(category.getId(), category.getName());
+    }
+}
--- main-service/src/main/java/ru/practicum/ewm/repository/UserRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package ru.practicum.ewm.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.model.user.User;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
